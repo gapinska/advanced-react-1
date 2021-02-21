a4eb6eea72f4615ab694cdfe6c7db40708baf735
Monika Gapińska <monika.gapinska@lppsa.com>
2021-02-21 22:54:26 +0100

    render props pattern

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import Movie from './components/Movie'
+import User from './components/User'
 
 function App() {
 	return (
 		<div className="App">
 			<Movie className="red" />
+			<User render={(isLoggedIn) => (isLoggedIn ? 'user' : 'guest')} />
 		</div>
 	)
 }
---
+++ react-advanced/src/components/User.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const User = ({ render }) => {
+	return <div>{render(false)}</div>
+}
+
+export default User
